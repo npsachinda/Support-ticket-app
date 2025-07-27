c4e53df8704bdf2e8b9c5f5b225b66a6437f3ab5
Nipun <nipun@docupath.ai>
2025-07-27 13:05:33 +0530

    Adding migrations, Models and controllers

---
+++ database/migrations/2025_07_27_071140_create_agents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('email',100);
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agents');
+    }
+};
