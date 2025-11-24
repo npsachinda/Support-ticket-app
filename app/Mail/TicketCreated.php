c8d211deda8482bfaca6dfb8e32f11779fc269b9
Nipun Liyanage <nipun.liyanage@elegantmedia.lk>
2025-11-24 16:37:28 +0530

    Update dependencies in composer.lock and package-lock.json; refactor TicketCreated class to implement ShouldQueue for improved email handling.

--- app/Mail/TicketCreated.php
+++ app/Mail/TicketCreated.php
@@ -8,8 +8,9 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class TicketCreated extends Mailable
+class TicketCreated extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
